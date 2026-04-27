78cf2e1bcf473c2494b0025541bdd0b211b540d4
Anuroopa-007 <anuroopa@tansam.org>
2026-04-27 14:00:20 +0530

    completed the login page backend frontend

---
+++ Frontend/Frontend/src/app/features/auth/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports:[CommonModule,FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+  error = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (res: any) => {
+        // Store in sessionStorage
+        sessionStorage.setItem('token', res.token);
+        sessionStorage.setItem('user', JSON.stringify(res.user));
+
+        // Navigate
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        this.error = err.error.message || 'Login failed';
+      }
+    });
+  }
+}
---
+++ Frontend/Frontend/src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private API = 'http://localhost:5000/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    return this.http.post(`${this.API}/login`, {
+      email,
+      password
+    });
+  }
+
+  register(data: any) {
+    return this.http.post(`${this.API}/register`, data);
+  }
+
+  logout() {
+    sessionStorage.clear();
+  }
+
+  getToken() {
+    return sessionStorage.getItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+}   
