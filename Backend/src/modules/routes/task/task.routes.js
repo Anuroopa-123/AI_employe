773c9393bad5783a8607a523d944d663c42eaff0
Anuroopa-007 <anuroopa@tansam.org>
2026-05-02 12:24:36 +0530

    the status progress the employe will update that is viewed by manager

--- Backend/src/modules/routes/task/task.routes.js
+++ Backend/src/modules/routes/task/task.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { assignTask, getMyTasks,getAssignedTasks,editTask,removeTask } from "../../controller/task/task.controller.js";
+import { assignTask, getMyTasks,getAssignedTasks,editTask,removeTask ,updateStatus} from "../../controller/task/task.controller.js";
 import { authMiddleware } from "../../../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.get('/assigned', authMiddleware, getAssignedTasks);
 router.get("/my-tasks", authMiddleware, getMyTasks);
 router.put("/update/:id", authMiddleware, editTask);
 router.delete("/delete/:id", authMiddleware, removeTask);
+router.put("/status/:id", authMiddleware, updateStatus);
 
 export default router;
